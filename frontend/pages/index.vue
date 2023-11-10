d2c5234e2a8a4cfad5b2f89f66693b645cbb9287
MichielMe <michiel.meire@hotmail.com>
2023-11-10 13:03:37 +0100

    first commit

---
+++ frontend/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum, alias eum? Deserunt ullam obcaecati labore provident. Tempore ex porro quibusdam.</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  p {
+    margin: 20px 0;
+  }
+</style>
